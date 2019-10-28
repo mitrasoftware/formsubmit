604b67a3ae30018431cdee6d58404a166b8925ce
irshadi049@gmail.com <irshadi049@gmail.com>
2019-10-28 12:49:34 +0530

    Updates

---
+++ precache-manifest.532a193114979749ef029fc6a67dbe9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "372ebf2918fef375531a2456c34cc6b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "150588ed73d786b726a7",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "5137fa4fa599a883c450",
+    "url": "/static/js/2.6a69793a.chunk.js"
+  },
+  {
+    "revision": "150588ed73d786b726a7",
+    "url": "/static/js/main.156d66da.chunk.js"
+  },
+  {
+    "revision": "9b9e094671edb534858e",
+    "url": "/static/js/runtime-main.c24655ad.js"
+  }
+]);
